4e290ccc564cadf29b29d7b5543896bc7a601a36
Martin Markov <martin.markov1980@gmail.com>
2023-11-21 11:16:55 +0000

    Section 2: Data types at Glance.

--- section_2/script.js
+++ section_2/script.js
@@ -1,14 +1,11 @@
-// This is single line comment
-// Always use const if the value should not be changed
-// Always use const if the type should not be changed (Arrays and Objects)
-const score = 100;
+// Data types at Glance
+let name = "Martin"; // String
+let score = 100; // Number
+let isWinner = true // Boolean
+let degree = null; // Null
+let degreeUndefined; // Undefined
+let scoreNumbers = [1, 2, 6, 10]; // Object such as array or object ex. const person = {firstName:"John", lastName:"Doe"} or const date = new Date("2022-03-25");
+const sym1 = Symbol(); // Symbol, Every Symbol() call is guaranteed to return a unique Symbol. const sym2 = Symbol("foo"); const sym3 = Symbol("foo"); Symbol("foo") === Symbol("foo"); // false
+let myNum = 9999999999999999n; // BigInt JavaScript BigInt variables are used to store big integer values that are too big to be represented by a normal JavaScript Number ex: let y = BigInt(1234567890123456789012345);
 
-// Only use let if you can't use const
-let updatedScore = 101;
-
-/* 
-This is multi line comment 
-Only use var if you MUST support old browsers.
-*/
-var name = "Martin";
 
