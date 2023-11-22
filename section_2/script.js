b6e9564aea0bea42fc5e91e948b40177d71c566f
Martin Markov <martin.markov1980@gmail.com>
2023-11-22 11:48:07 +0000

    Section 2: null & undefined.

--- section_2/script.js
+++ section_2/script.js
@@ -1,25 +1,6 @@
-// arrays
+// null & undefined
+let age;
+console.log(age, age + 3, `the age is ${age}`); // undefined NaN the age is undefined
 
-let ninjas = ['shaun', 'ryu', 'chun-li'];
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-
-// array length
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join(',');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken');
-result = ninjas.pop();
-
-console.log(result);
-console.log(ninjas);
+age = null;
+console.log(age, age + 3, `the age is ${age}`); // null 3 the age is null
