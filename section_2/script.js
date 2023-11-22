9c467df5f296aa9e9bbef95a76a911e97f5be923
Martin Markov <martin.markov1980@gmail.com>
2023-11-22 09:54:53 +0000

    Section 2: Strings

--- section_2/script.js
+++ section_2/script.js
@@ -1,11 +1,15 @@
-// Data types at Glance
-let name = "Martin"; // String
-let score = 100; // Number
-let isWinner = true // Boolean
-let degree = null; // Null
-let degreeUndefined; // Undefined
-let scoreNumbers = [1, 2, 6, 10]; // Object such as array or object ex. const person = {firstName:"John", lastName:"Doe"} or const date = new Date("2022-03-25");
-const sym1 = Symbol(); // Symbol, Every Symbol() call is guaranteed to return a unique Symbol. const sym2 = Symbol("foo"); const sym3 = Symbol("foo"); Symbol("foo") === Symbol("foo"); // false
-let myNum = 9999999999999999n; // BigInt JavaScript BigInt variables are used to store big integer values that are too big to be represented by a normal JavaScript Number ex: let y = BigInt(1234567890123456789012345);
+// Strings
+let firstName = "Martin";
+let lastName = "Markov";
 
+// string concatenation
+let fullName = firstName + " " + lastName; 
+console.log(fullName); // Martin Markov
+// getting characters
+console.log(fullName[1]); // a
 
+// string property length 
+console.log(fullName.length); // 13
+
+// string methods
+console.log(fullName.toUpperCase()); // MARTIN MARKOV
