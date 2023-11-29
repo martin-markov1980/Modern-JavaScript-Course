7dd102ff5668b2a993ad676426c640a8f4d32035
Martin Markov <martin.markov1980@gmail.com>
2023-11-29 13:39:30 +0000

    Section 4: Arrow functions.

--- section_4/script.js
+++ section_4/script.js
@@ -11,7 +11,7 @@ const calcArea = radius => 3.14 * radius**2;
 const area = calcArea(5);
 console.log('area is:', area);
 
-// practise arrow functions
+// practice arrow functions
 
 // const greet = function(){
 //   return 'hello, world';
